c8b1b2cdfe46c7fff9760a41ed8a6d0f6d31622e
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-03-19 19:05:42 +0100

    ✅ Buying tokens for ETH - correct accounting

--- src/race_01/crytic/EchidnaTest.sol
+++ src/race_01/crytic/EchidnaTest.sol
@@ -16,6 +16,8 @@ import {PropertiesAsserts} from "./utils/PropertiesHelper.sol";
 /// with all the contracts deployed with `forge script`.
 contract EchidnaTest is Addresses, PropertiesAsserts {
 
+    event SenderBalance(uint256 balance);
+
     function total_supply_should_be_lte_max_supply() public {
         uint256 totalSupply = token.totalSupply();
         uint256 maxSupply = 100 ether;
@@ -38,5 +40,31 @@ contract EchidnaTest is Addresses, PropertiesAsserts {
         assertEq(senderTokenBalanceBefore, senderTokenBalanceAfter, "Token balance should not change.");
     }
 
+    function buying_tokens_should_increase_balance(uint256 desired_tokens) public payable {
+        // preconditions
+        uint256 senderEtherBalanceBefore = msg.sender.balance;
+        uint256 senderTokenBalanceBefore = token.balances(msg.sender);
+        uint256 ether_sent = msg.value;
+        emit SenderBalance(senderEtherBalanceBefore);
 
+        // action
+        try token.buy{value: ether_sent}(desired_tokens) {
+            // token.buy is successful
+            // postconditions
+            require(desired_tokens > 0 && ether_sent > 0);
+            uint256 senderEtherBalanceAfter = msg.sender.balance;
+            uint256 senderTokenBalanceAfter = token.balances(msg.sender);
+            assertEq(senderEtherBalanceBefore - ether_sent, senderEtherBalanceAfter, "Ether balance should decrease.");
+            assertEq(
+                senderTokenBalanceBefore + desired_tokens, senderTokenBalanceAfter, "Token balance should increase."
+            );
+        } catch {
+            // token.buy failed
+            // postconditions
+            uint256 senderEtherBalanceAfter = msg.sender.balance;
+            uint256 senderTokenBalanceAfter = token.balances(msg.sender);
+            assertEq(senderEtherBalanceBefore, senderEtherBalanceAfter, "Ether balance should not change.");
+            assertEq(senderTokenBalanceBefore, senderTokenBalanceAfter, "Token balance should not change.");
+        }
+    }
 }
