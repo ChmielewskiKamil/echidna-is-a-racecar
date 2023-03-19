628db92697fb117b109334d54c53c1c9f7854ea7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-03-19 15:19:18 +0100

    📝 Add NatSpec comments explaining Echidna setup

--- src/race_01/crytic/EchidnaTest.sol
+++ src/race_01/crytic/EchidnaTest.sol
@@ -2,13 +2,19 @@ pragma solidity 0.7.0;
 
 /// @dev The Addresses.sol file contains a list of contract addresses
 /// deployed to anvil with the deployment script.
-/// Thanks to this approach we do not have to deploy anything 
+/// Thanks to this approach we do not have to deploy anything
 /// in the EchidnaTest contract (or EchidnaSetup contract).
+/// Addresses.sol also imports all of the necessary project contracts.
+/// That's why we don't have to explicitly import them here.
 import {Addresses} from "./utils/Addresses.sol";
 
+/// @dev Run Echidna tests with:
+/// echidna src/race_01/crytic/EchidnaTest.sol --contract EchidnaTest --config src/race_01/crytic/echidna_config.yaml
+/// @notice Look at the .env file. You need to set the Echidna RPC settings,
+/// for the fork mode to work. Also make sure that you have Anvil running,
+/// with all the contracts deployed with `forge script`.
 contract EchidnaTest is Addresses {
-  function token_should_be_deployed() public view {
-    assert(address(token) != address(0));
-  }
+    function token_should_be_deployed() public view {
+        assert(address(token) != address(0));
+    }
 }
-
