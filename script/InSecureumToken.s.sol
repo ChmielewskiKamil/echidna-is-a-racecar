3e0db68672bb9d7d226cf8417b481b2bdc9a2e7c
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-03-19 14:16:00 +0100

    👷 Add InSecureumToken deployment script

---
+++ script/InSecureumToken.s.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.7.0;
+
+import "forge-std/Script.sol";
+import {InSecureumToken} from "src/race_01/InSecureumToken.sol";
+
+contract Deployment is Script {
+    // Deploy to Anvil with:
+    // forge script script/InSecureumToken.s.sol:Deployment --fork-url http://localhost:8545 --broadcast
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        InSecureumToken token = new InSecureumToken();
+        vm.stopBroadcast();
+    }
+}
